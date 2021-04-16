deaf993b1c1971b1adb79be2d62955dd59f06028
nicholas.chang <nicholas.chang@cloudtechgenius.com>
2021-04-16 22:36:42 +0100

    changes updated

--- static/css/main.js
+++ static/css/main.js
@@ -2,20 +2,20 @@ window.addEventListener('DOMContentLoaded', (event) => {
     getVisitCount();
 });
 
-const apiGateway = 'https://func-prd01.azurewebsites.net/api/HttpTrigger1?code=jD5KMt7q41qzs1IqThUL8otKIan60ad7uyk4FZ9Ews/hzK0om6t5JA==';
+const apiGateway = 'https://func-prd01.azurewebsites.net/api/HttpTrigger1?code=R/xuV1h6W9lc00m7hqiYYKMbxP70uGqi9OabEcuJbM/SBrntqt80PA==';
 
 const getVisitCount = () => {
-    let count = 0;
-    fetch(apiGateway, {
-        mode: 'cors',
-    })
+    let count = 30;
+    fetch(functionApi)
     .then(response => {
         return response.json()
     })
-    .then(res => {
-        const count = res.Attributes.visitcount;
-        document.getElementById('counter').innerText =count;
-        document.getElementById('visitorElem').style.display = 'block';
-    })
+    .then(response => {
+        console.log("Website called function API.");
+        count = response.count;
+        document.getElementById('counter').innerText = count;
+    }).catch(function(error) {
+        console.log(error);
+      });
     return count;
 }
