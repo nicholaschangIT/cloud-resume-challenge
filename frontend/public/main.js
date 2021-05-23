7f12f8d24b29883e8d25e2a55e0cee3fdf316d60
nicholas.chang <nicholas.chang@cloudtechgenius.com>
2021-05-23 12:57:02 +0100

    commit

--- frontend/public/main.js
+++ frontend/public/main.js
@@ -1,21 +1,20 @@
-window.addEventListener('DOMContentLoaded', (event) => {
-    getVisitCount();
+window.addEventListener('DOMContentLoaded', (event) =>{
+  getVisitCount();
+})
+
+const functionApiUrl = 'https://getresumecounter.azurewebsites.net/api/GetResumeCounter?code=Q/LkPt0mhQKdP8DoE3DdllAYdFQO//58Iq7AoS6JUBj9FgJ86Rqt8A==';
+const localFunctionApi = 'http://localhost:7071/api/GetResumeCounter';
+
+const getVisitCount = () => {
+  let count = 30;
+  fetch(functionApiUrl).then(response => {
+      return response.json()
+  }).then(response =>{
+      console.log("Website called function API.");
+      count =  response.count;
+      document.getElementById("counter").innerText = count;
+  }).catch(function(error){
+      console.log(error);
   });
-  
-  const functionApi = 'https://rishabresume.azurewebsites.net/api/HttpTrigger1?code=HDldDyHHM2jSTsRA283DMOL9FYdlsJYXg5Z3Hie25d7aYjWwaMsqTw==';
-  
-  const getVisitCount = () => {
-    let count = 7;
-    fetch(functionApi)
-      .then(response => {
-        return response.json()
-      })
-      .then(response => {
-        count = response;
-        console.log("Hello 👋, you are visitor number - " + count);
-        document.getElementById('counter').innerText = count;
-      }).catch(function (error) {
-        console.log(error);
-      });
-    return count;
-  }
+  return count;
+}
